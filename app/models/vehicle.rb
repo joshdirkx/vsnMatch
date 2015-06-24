60dcd4b64baedb2ff1917aa81e2a2677e311a544
Joshua Dirkx <joshdirkx@gmail.com>
2015-06-24 17:17:34 -0500

    models annotated

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id          :integer          not null, primary key
+#  vsn_pattern :text
+#  trim_id     :integer
+#  year        :integer
+#  make        :string
+#  model       :string
+#  trim_name   :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Vehicle < ActiveRecord::Base
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id          :integer          not null, primary key
+#  vsn_pattern :text
+#  trim_id     :integer
+#  year        :integer
+#  make        :string
+#  model       :string
+#  trim_name   :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
