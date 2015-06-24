0b5983f031f68fe0c7868e6a0bf9c737dc777aee
Joshua Dirkx <joshdirkx@gmail.com>
2015-06-24 17:24:47 -0500

    seed method created and database seeded

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -14,4 +14,17 @@
 #
 
 class Vehicle < ActiveRecord::Base
+  def self.seed_vehicle_information(file)
+    data = Roo::CSV.new(file)
+    data.each do |i|
+      vehicle_information             = Vehicle.new
+      vehicle_information.vsn_pattern = i[0]
+      vehicle_information.trim_id     = i[1]
+      vehicle_information.year        = i[2]
+      vehicle_information.make        = i[3]
+      vehicle_information.model       = i[4]
+      vehicle_information.trim_name   = i[5]
+      vehicle_information.save
+    end
+  end
 end
