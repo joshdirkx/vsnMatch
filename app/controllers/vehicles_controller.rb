7ff65e6df659aaec55889494c3f871e906f3cd70
Joshua Dirkx <joshdirkx@gmail.com>
2015-06-26 21:40:30 -0500

    if car_match_id hash == results, then no match is better than the other. show all matches.

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,13 +2,12 @@ class VehiclesController < ApplicationController
   include VSN_Format_Validation
 
   def search
-    if !vsn_length_check(params[:search])
-      flash[:notice] = 'Invalid Length'
-      redirect_to root_path
-    else !vsn_first_half_check(params[:search]) || !vsn_second_half_check(params[:search])
-      flash[:notice] = 'Incorrect Format'
-      redirect_to root_path
-    end
+    # if vsn_length_check(params[:search]) && vsn_first_half_check(params[:search]) && vsn_second_half_check(params[:search])
+
+    # else
+    #   flash[:notice] = 'Incorrect Format'
+    #   redirect_to root_path
+    # end
 
     @found_match = nil
 
@@ -16,14 +15,28 @@ class VehiclesController < ApplicationController
 
     @results = Vehicle.car_search(params[:search])
 
-    @most_likely_match = Vehicle.most_specific_vsn_match(@results)
+    car_match_id_hash = Vehicle.most_specific_vsn_match(@results)
 
-    if @most_likely_match.count == 1
-      @most_likely_match = Vehicle.find(@most_likely_match.first[0])
-      @found_match = true
-    else
+    @best_match = []
+
+    if car_match_id_hash.count == @results.count
       @found_match = false
+    else
+      car_match_id_hash.each do |item|
+        @best_match.push(Vehicle.find(item[0]))
+        @found_match = true
+      end
     end
+    # if @most_likely_match.count == 1
+    # 	@most_likely_match = Vehicle.find(@most_likely_match.first[0])
+    # 	@found_match = true
+    # 	els
+    # if @most_likely_match.count == 1
+    #   @most_likely_match = Vehicle.find(@most_likely_match.first[0])
+    #   @found_match = true
+    # else
+    #   @found_match = false
+    # end
   end
 
   def landing
